f06fb88b45e6f8901ae56bf2e41f630dff3e13ca
NathanPakenham1 <nathan@nathanpakenham.com>
2024-12-31 09:41:24 +0000

    Decorator Pattern Explanation

--- index.php
+++ index.php
@@ -1,55 +1,48 @@
 <?php
 
-/*
- *
- * MediaPlayer Interface: Defines the method to play audio.
-   AdvancedMediaPlayer Interface: Defines methods to play different formats.
-   VlcPlayer and Mp4Player Classes: Implement the AdvancedMediaPlayer interface.
-   MediaAdapter Class: Implements the MediaPlayer interface and uses AdvancedMediaPlayer to play different formats.
-   AudioPlayer Class: Uses MediaAdapter to play different formats.
-   Client Code: Demonstrates how the client uses the AudioPlayer to play different audio formats.
- *
- */
-
-interface MediaPlayer {
-    public function play(string $audioType, string $fileName): void;
+interface Coffee {
+    public function getCost(): float;
+    public function getDescription(): string;
 }
 
-class VlcPlayer implements MediaPlayer {
-    public function play(string $audioType, string $fileName): void {
-        if ($audioType == 'vlc') {
-            echo 'Playing vlc file name:' . $fileName . '<br>';
-        }
+class SimpleCoffee implements Coffee {
+    public function getCost(): float {
+        return 10;
+    }
+
+    public function getDescription(): string
+    {
+        return 'Simple Coffee';
     }
 }
 
-class Mp4Player implements MediaPlayer {
-    public function play(string $audioType, string $fileName): void {
-        if ($audioType == 'mp4') {
-            echo 'Playing mp4 file name:' . $fileName . '<br>';
-        }
+abstract class CoffeeDecorator implements Coffee {
+    protected $coffee;
+
+    public function __construct(Coffee $coffee) {
+        $this->coffee = $coffee;
+    }
+
+    public function getCost(): float {
+        return $this->coffee->getCost();
+    }
+
+    public function getDescription(): string {
+        return $this->coffee->getDescription();
     }
 }
 
-class AudioPlayer implements MediaPlayer {
-    private $mediaAdapter;
+class MilkCoffee extends CoffeeDecorator {
+    public function getCost(): float
+    {
+        return $this->coffee->getCost();
+    }
 
-    public function play(string $audioType, string $fileName): void {
-        if ($audioType == 'mp3') {
-            echo 'Playing mp3 file name:' . $fileName . '<br>';
-        } elseif ($audioType == 'vlc') {
-            $this->mediaAdapter = new VlcPlayer();
-            $this->mediaAdapter->play($audioType, $fileName);
-        } elseif ($audioType == 'mp4') {
-            $this->mediaAdapter = new Mp4Player();
-            $this->mediaAdapter->play($audioType, $fileName);
-        } else {
-            echo 'Invalid audio file type:' . $audioType . '<br>';
-        }
+    public function getDescription(): string {
+        return $this->coffee->getDescription() . ', milk';
     }
 }
 
-$audioPlayer = new AudioPlayer();
-
-$audioPlayer->play('mp3', 'test.mp3');
-
+$coffee = new SimpleCoffee();
+$MilkCoffee = new MilkCoffee($coffee);
+echo $MilkCoffee->getCost();
